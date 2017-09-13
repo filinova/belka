23971eb4c97f37240890ebfd00a1323a7d25f557
filinova <nes.impress@gmail.com>
2017-09-13 17:42:52 +0300

    Изменение разметки, подключение слайдера

--- js/script.js
+++ js/script.js
@@ -1,7 +1,46 @@
-console.log('js work!');
-
 
 $(document).ready(function(){
-  console.log('jQ work!');
-  $('.slick__wrap').slick();
+  var gallery = $('.gallery__slick');
+
+  function slickUp(val) {
+    $(val).slick({
+      mobileFirst: true,
+      slidesToShow: 1,
+      slidesToScroll: 1
+    });
+  }
+  function slickDown(val){
+    $(val).slick("unslick");
+  }
+
+  if ($(window).width() < 768 ) {
+    slickUp(gallery);
+  }
+
+  $(window).resize(function(){
+
+    if ( $(window).width() < 768 ) {
+      
+      if ( $(gallery).hasClass("slick-initialized") ) {
+        return true;
+      } else {
+        slickUp(gallery);
+      }
+
+    } else if ( $(window).width() >= 768 ){
+
+      if ( $(gallery).hasClass("slick-initialized") ) {
+        slickDown(gallery);
+      }
+    
+    }
+  });
+    $('.review__slick').slick({
+    
+  });
+
+    $('.trails__slick').slick({
+    
+  });
+
 });
